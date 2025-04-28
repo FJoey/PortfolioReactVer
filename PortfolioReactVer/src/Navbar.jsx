97b1cfaea69b9292d1afe81e7c5eaf5f69e5dc4f
FunctionJoey <functionjoey@gmail.com>
2025-04-28 12:33:31 -0600

    Added base items

---
+++ PortfolioReactVer/src/Navbar.jsx
@@ -0,0 +1,20 @@
+
+
+function Navbar(){
+
+    return(
+        <>
+            <nav>
+
+                    <a href="#"> J.M. Harrington</a>
+                    <a href="#"> About Me</a>
+                    <a href="#"> Portfolio Collection</a>
+                    <a href="#"> Interactive Section</a>
+            </nav>
+            <hr></hr>
+        </>
+    );
+
+}
+
+export default Navbar
